3083c9ff6878ad9090d4e3ea838a575cdb857724
sam <sam@192.168.0.12>
2020-09-05 22:02:57 -0400

    Added addition and multiplication

--- Helloworld/src/Helloworld.java
+++ Helloworld/src/Helloworld.java
@@ -4,6 +4,11 @@ public class Helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello world");
+	 int a=8, b=9;
+	 int c=a+b;
+	 int d=a*b;
+	 System.out.println("Addition is " + c +"and multiplication is " + d);
+	
 	}
 
 }
